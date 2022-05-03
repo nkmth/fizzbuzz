9e9f33f53ce061f0c4b26efda1f5f3797a96c503
nkmth <rairai30@gmail.com>
2022-05-03 09:24:05 +0900

    Add fizzbuzz command

---
+++ cmd/fizzbuzz/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/nakmo/fizzbuzz"
+)
+
+func main() {
+	flag.Parse()
+
+	arg, err := strconv.Atoi(flag.Arg(0))
+	if err != nil {
+		fmt.Println("Argument is only number.")
+		os.Exit(1)
+	}
+
+	ret, _ := fizzbuzz.Convert(arg)
+	fmt.Println(ret)
+}
